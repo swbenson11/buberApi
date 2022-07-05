691b60bb2bfbf6c0745ebf7ad3fc2a769effbf3f
swbenson11 <swbenson11@gmail.com>
2022-07-05 10:04:05 -0600

    Error handling via filter

--- BuberDinner.api/Program.cs
+++ BuberDinner.api/Program.cs
@@ -1,4 +1,6 @@
 
+using BuberDinner.Api.Filters;
+using BuberDinner.Api.Middleware;
 using BuberDinner.application;
 using BuberDinner.infrastructure;
 
@@ -8,12 +10,16 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddBuberApplication();
     builder.Services.AddBuberInfrastructure(builder.Configuration);
 
-    builder.Services.AddControllers();
+// Error option 1
+    builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 
 }
 var app = builder.Build();
+
+// Error option 2
+// app.UseMiddleware<ErrorHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
